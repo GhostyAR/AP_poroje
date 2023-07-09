64ccdfadf2198162339ddf6685a1fde775a48cac
AH <alihamzehiesfahani@gmail.com>
2023-07-09 23:11:20 +0430

    add the html urls to the blog

--- torobche/blog/urls.py
+++ torobche/blog/urls.py
@@ -5,4 +5,11 @@ from . import views
 urlpatterns = [
     path('', views.home, name='blog-home'),
     path('about/', views.about, name='blog-about'),
+    path('search/', views.search, name='search'),
+    path('laptop/', views.laptop, name='laptop'),
+    path('mobile/', views.mobile, name='mobile'),
+    path('smart_watch/', views.smart_watch, name='smart_watch'),
+    path('washing_machine/', views.washing_machine, name='washing_machine'),
+    path('tablet/', views.tablet, name='tablet'),
+    path('product/<pk>', views.mobile, name='mobile'),
 ]
